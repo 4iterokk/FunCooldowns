b870c878212c77a0696f7afa42c71bc83220c559
4iterokk <bsspike170im@gmail.com>
2025-09-19 20:57:34 +0300

    Исправлена [BUTTON] на 1.21.4+

--- src/main/java/me/jetby/cmdTimer/manager/Actions.java
+++ src/main/java/me/jetby/cmdTimer/manager/Actions.java
@@ -13,6 +13,7 @@ import org.bukkit.potion.PotionEffectType;
 import static me.jetby.cmdTimer.utils.Config.CFG;
 import static me.jetby.cmdTimer.utils.Parser.color;
 import static me.jetby.cmdTimer.utils.Parser.hex;
+//
 
 public class Actions {
 
